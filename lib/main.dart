55bc32a208f02ac4127a5bc3ecfcca1f078456e4
kavyapandey <kavyapandey3ap@gmail.com>
2022-03-02 17:12:21 +0530

    day 3 compeleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_catolge/pages/home_page.dart';
+import 'package:flutter_catolge/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,14 +11,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //build is a function which we are overriding
+    //Build Context ia a tree app
+    //context is a piece of inf. what lyed on where
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+
+        //In map there is a key this is called dictionary
+        //Map format is like json
       ),
+      initialRoute: "/home", // by default home will open
+      routes: {
+        //"/" means default entry of home page in route so we dont have to write about homepage again
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
+
+//This is called method
+//bringVegetable(){
+//take cycke
+
+//Go to sec 16
+
+//}// These thing are called method
+//..
+///////////////////////////////////
+/////Theme Data is also a class
